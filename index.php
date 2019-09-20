8b9079a7edf01a40d42150aa6abed35ae32c55d7
kwang yoon kim <naribi3@naver.com>
2019-09-20 10:40:42 +0900

    test

--- index.php
+++ index.php
@@ -1,3 +1,23 @@
-<?php
-    echo phpinfo();
-?>
+<html>
+<head>
+<script src="https://cdn.jsdelivr.net/npm/vue"></script>
+</head>
+
+<body>
+
+<div id="app">
+  {{ message }}
+</div>
+
+<script language='javascript'>
+var app = new Vue({
+  el: '#app',
+  data: {
+    message: '안녕하세요 Vue!'
+  }
+})
+</script>
+
+</body>
+
+</html>
