dc4bc764cbb0bb4b57d46ba9724dad913ce84211
kwang yoon kim <naribi3@naver.com>
2019-09-20 12:04:11 +0900

    vue 테스트

--- index.php
+++ index.php
@@ -1,6 +1,19 @@
 <html>
 <head>
 <script src="https://cdn.jsdelivr.net/npm/vue"></script>
+</head>
+
+<body>
+
+<div id="app">
+  {{ message }}
+</div>
+
+<div id="app-2">
+  <span v-bind:title="message">
+    내 위에 잠시 마우스를 올리면 동적으로 바인딩 된 title을 볼 수 있습니다!
+  </span>
+</div>
 
 <script language='javascript'>
 var app = new Vue({
@@ -9,14 +22,14 @@ var app = new Vue({
     message: '안녕하세요 Vue!'
   }
 })
+
+var app2 = new Vue({
+  el: '#app-2',
+  data: {
+    message: '이 페이지는 ' + new Date() + ' 에 로드 되었습니다'
+  }
+})
 </script>
-</head>
-
-<body>
-
-<div id="app">
-  {{ message }}
-</div>
 
 </body>
 </html>
